5d4cfc4d669af630b216e603d9a8d8487083068f
Dual <Dual@TITAN10.sinf2003srvr2.local>
2018-01-08 16:25:13 +0100

    javascript horario

---
+++ javaS5.js
@@ -0,0 +1,110 @@
+function BDOVER(obj) {
+	obj.style.color = "red";
+	obj.style.fontWeight = "bold";
+}
+
+function BDOUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function BDOWN(obj) {
+	document.getElementById("descripcion").style.color = "black";
+	document.getElementById("descripcion").innerHTML = "Descripcion Bases de datos: Nos centramos en el manejo de las bases mediante SQL, mySQL y en un principio Algebra relational.";
+}
+
+function BDUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+function PROVER(obj) {
+	obj.style.color = "green";
+	obj.style.fontWeight = "bold";
+}
+
+function PROUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function PRDOWN(obj) {
+	document.getElementById("descripcion").style.color = "orange";
+	document.getElementById("descripcion").innerHTML = "Descripcion Programacion: Nos centramos en aprender lenguajes de programacion como Python y Java para crear programas.";
+}
+
+function PRUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+function SIOVER(obj) {
+	obj.style.color = "blue";
+	obj.style.fontWeight = "bold";
+}
+
+function SIOUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function SIDOWN(obj) {
+	document.getElementById("descripcion").style.color = "yellow";
+	document.getElementById("descripcion").innerHTML = "Descripcion Sistemas operativos: Nos centramos en aprender conceptos básicos. Tipos y estructuras de SSOO.";
+}
+
+function SIUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+function EDOVER(obj) {
+	obj.style.color = "yellow";
+	obj.style.fontWeight = "bold";
+}
+
+function EDOUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function EDOWN(obj) {
+	document.getElementById("descripcion").style.color = "yellow";
+	document.getElementById("descripcion").innerHTML = "Descripcion Entornos de desarrollo: Nos centramos en maximizar la productividad del programador proporcionando componentes muy unidos con interfaces de usuario similares.";
+}
+
+function EDUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+function LMOVER(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "bold";
+}
+
+function LMOUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function LMDOWN(obj) {
+	document.getElementById("descripcion").style.color = "black";
+	document.getElementById("descripcion").innerHTML = "Descripcion Lenguaje de marcas: Nos centramos en desarrollo web, lo primero que aprendemos es el lenguage de marcas más extendido: HTML.";
+}
+
+function LMUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+function FTOVER(obj) {
+	obj.style.color = "orange";
+	obj.style.fontWeight = "bold";
+}
+
+function FTOUT(obj) {
+	obj.style.color = "black";
+	obj.style.fontWeight = "normal";
+}
+
+function FTDOWN(obj) {
+	document.getElementById("descripcion").style.color = "orange";
+	document.getElementById("descripcion").innerHTML = "No tengo ninguna clase a esta hora. ";
+}
+
+function FTUP(obj) {
+	document.getElementById("descripcion").innerHTML = ""
+}
+
+
