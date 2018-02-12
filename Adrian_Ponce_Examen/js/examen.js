637b29d6160ed94e34df5993cb3bf554762beef5
Adrián Ponce Cano <adrianponcecano@gmail.com>
2018-02-12 21:32:46 +0100

    Examen_Rec

---
+++ Adrian_Ponce_Examen/js/examen.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+}
+function myFunction() {
+
+  document.getElementById("evoEuribor").style.display = "block";
+  document.getElementById("economia").style.display = "block";
+}
+function economia() {
+
+  document.getElementById("economia").style.display = "block";
+  document.getElementById("evoEuribor").style.display = "none";
+}
+function euribor() {
+
+  document.getElementById("economia").style.display = "none";
+  document.getElementById("evoEuribor").style.display = "block";
+}
+
+
