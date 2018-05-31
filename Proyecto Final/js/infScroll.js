36603ba53c3277cf4ad54466a1dccc5bf27fc222
Adrián Ponce Cano <adrianponcecano@gmail.com>
2018-05-31 14:20:12 +0200

    infinite scroll

--- Proyecto Final/js/infScroll.js
+++ Proyecto Final/js/infScroll.js
@@ -1,4 +1,5 @@
 var NOTICIA1;
+var NOTICIA2;
 
 window.onload = function() {
 
@@ -6,12 +7,13 @@ window.onload = function() {
       //En este punto el objeto jsonObject corresponde al fichero
      
       NOTICIA1 = jsonObject;
-
+      console.log(NOTICIA1);
     });
 
    $.getJSON( "./json/news2.json", function( jsonObject ) {
     //En este punto el objeto jsonObject corresponde al fichero
-      visualize( jsonObject );
+      NOTICIA2 = jsonObject;
+      console.log(NOTICIA2);
     });
 
 
@@ -22,12 +24,13 @@ $(window).on("scroll", function() {
   var scrollPosition = $(window).height() + $(window).scrollTop();
   if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
     visualize(NOTICIA1);
+    visualize(NOTICIA2);
   }
 });
 
-// Pintamos los ficheros json en html 
+// Visualizamos los ficheros json en la pagina
 function visualize(jsonObject){
-  $.each( jsonObject, function( i, noticia ) {
-    $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
+  $.each( jsonObject, function( i, news ) {
+    $("#see").append( "<h1 id='" + i + "'>" + news.titular+ "</h1>" + "<p>" + news.descripcion + "</p>" + "<p>" + news.descripcion + "</p>"  );
   }); 
 }
--- Proyecto Final/js/readMore.js
+++ Proyecto Final/js/readMore.js
@@ -1,23 +1,33 @@
+var NOTICIA1;
 
 window.onload = function() {
 
-function leerArchivo(e) {
-  var archivo = e.target.files[0];
-  if (!archivo) {
-    return;
+   $.getJSON( "./json/news1.json", function( jsonObject ) {
+      //En este punto el objeto jsonObject corresponde al fichero
+     
+      NOTICIA1 = jsonObject;
+
+    });
+
+   $.getJSON( "./json/news2.json", function( jsonObject ) {
+    //En este punto el objeto jsonObject corresponde al fichero
+      visualize( jsonObject );
+    });
+
+
+};
+
+$(window).on("click", function() {
+  var scrollHeight = $(document).height();
+  var scrollPosition = $(window).height() + $(window).scrollTop();
+  if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+    visualize(NOTICIA1);
   }
-  var lector = new FileReader();
-  lector.onload = function(e) {
-    var contenido = e.target.result;
-    mostrarContenido(contenido);
-  };
-  lector.readAsText(archivo);
-}
+});
 
-function mostrarContenido(contenido) {
-  var elemento = document.getElementById('contenido-archivo');
-  elemento.innerHTML = contenido;
+// Pintamos los ficheros json en html 
+function visualize(jsonObject){
+  $.each( jsonObject, function( i, news ) {
+    $("#see").append( "<h1 id='" + i + "'>" + news.titular+ "</h1>" + "<p>" + news.descripcion + "</p>"  );
+  }); 
 }
-
-document.getElementById('file-input')
-  .addEventListener('change', leerArchivo, false);
