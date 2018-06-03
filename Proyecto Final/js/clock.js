9fe1a819df33406f62a07dd3818874cf074b9565
Adrián Ponce Cano <adrianponcecano@gmail.com>
2018-06-03 12:05:51 +0200

    clock

--- Proyecto Final/js/clock.js
+++ Proyecto Final/js/clock.js
@@ -2,51 +2,53 @@ window.onload = function() {
 	actualizeHour();
 }
 	
+  
 function actualizeHour() {
-	var date = new Date();
+  var date = new Date();
 
-	var hours = date.getHours();
+  var hours = date.getHours();
 
-	var minutes = date.getMinutes();
+  var minutes = date.getMinutes();
 
-	var seconds = date.getSeconds();
+  var seconds = date.getSeconds();
 
-	var day = date.getDate();
+  var day = date.getDate();
 
-	var month = date.getMonth();
+  var month = date.getMonth();
 
-	var year = date.getFullYear();
-
-
-	var month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  var year = date.getFullYear();
 
 
-	document.getElementById("day").innerHTML = date.getDate();
-	document.getElementById("month").innerHTML = [month[date.getMonth()]];
-	document.getElementById("year").innerHTML = date.getFullYear();
-	document.getElementById("hours").innerHTML = hours;
-	document.getElementById("minutes").innerHTML = minutes;
-	document.getElementById("seconds").innerHTML = seconds;
+  var month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+
+  document.getElementById("day").innerHTML = date.getDate();
+  document.getElementById("month").innerHTML = [month[date.getMonth()]];
+  document.getElementById("year").innerHTML = date.getFullYear();
 
 
 
-	console.log('date' + date.getDate());
-	console.log('month' + date.getMonth());
-	console.log('year' + date.getFullYear());
+  if (hours >= 12){
+    var ampm = 'PM';
+  } else {
+    var ampm = 'AM';
+  }
 
+  if (hours == 0){
+    hours = 12;
+  }
 
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("ampm").innerHTML = ampm;
 
-	if (hours >= 12){
-		var ampm = 'PM';
-	} else {
-		var ampm = 'AM';
-	}
+  if (minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10){
+      seconds = "0" + seconds;
+    }
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 
-	if (hours == 0){
-		hours = 12;
-	}
-
-	document.getElementById("ampm").innerHTML = ampm;
-
-	setTimeout(actualizeHour, 1000);
+  setTimeout(actualizeHour, 1000);
 }
